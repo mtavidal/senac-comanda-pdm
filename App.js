837aca9096d4f3213dc9b2cf4a29537cf790526d
mtavidal <119874263+mtavidal@users.noreply.github.com>
2024-02-13 21:04:23 -0300

    Adiciona tela login

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Sistema</Text>
+      <Text style={styles.title}>Comanda</Text>
+      <TextInput placeholder='e-mail' style={styles.input} />
+      <TextInput placeholder='senha' style={styles.input} secureTextEntry />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fcfbff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#121a2c',
+    fontSize: 24,
+    fontWeight: 'bold'
+
+  },
+  input: {
+    padding: 8,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginTop: 20,
+    borderRadius: 4
+  }
 });
